9d102ad6500e3017acfa6eb565b5bfefa9e12073
Rb Bharti <47843771+bhartiRB@users.noreply.github.com>
2021-09-07 21:20:22 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -5,4 +5,5 @@ ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
     print("hello world")
+    print("My name is robert")
     return {"statusCode": 200, "body": json.dumps(response)}
